744551d8975714850659e7e3e8c28d32d03b6c17
unknown <bitapa123@gmail.com>
2020-10-30 17:25:43 +0200

    Task1 and Task2 finished, pageobject approach

--- src/test/java/stepDefinitions/JobListSteps.java
+++ src/test/java/stepDefinitions/JobListSteps.java
@@ -60,12 +60,66 @@ public class JobListSteps {
         jobPage.enterNameAndJob(name,job);
         driver.findElement(By.xpath("//*[@id=\"modal_button\"]")).click();
     }
-
-
     @Then("^First persons name is \"([^\"]*)\" and job is \"([^\"]*)\"$")
     public void firstPersonsNameIsAndJobIs(String name, String job) throws Throwable {
         List<WebElement> links = driver.findElements(By.xpath("//li[contains(@id, \"person\")]"));
         assertEquals(name, links.get(0).findElement(By.xpath("//span[3]")).getText());
         assertEquals(job, links.get(0).findElement(By.xpath("//span[4]")).getText());
+    }
+
+    @And("^After adding person with \"([^\"]*)\" and \"([^\"]*)\" is on the page\\.$")
+    public void afterActionPersonWithAndStatusOnThePage(String name, String job) throws Throwable {
+        assertEquals(name, driver.findElement(By.xpath("//*[@id=\"person3\"]/span[3]")).getText());
+        assertEquals(job, driver.findElement(By.xpath("//*[@id=\"person3\"]/span[4]")).getText());
+    }
+    @And("^After editing person with \"([^\"]*)\" and \"([^\"]*)\" is on the page\\.$")
+    public void afterActionPersonWithAndStatusIsOnThePage(String name, String job) throws Throwable {
+        assertEquals(name, driver.findElement(By.xpath("//*[@id=\"person0\"]/span[3]")).getText());
+        assertEquals(job, driver.findElement(By.xpath("//*[@id=\"person0\"]/span[4]")).getText());
+    }
+    @And("^After deleting person with \"([^\"]*)\" and \"([^\"]*)\" is not on the page\\.$")
+    public void afterActionPersonWithAndStatusIsNotOnThePage(String name, String job) throws Throwable {
+        assertEquals(0, driver.findElements(By.id("person3")).size());
+    }
+
+    @And("^I click reset original list$")
+    public void iClickResetOriginalList() {
+        driver.findElement(By.xpath("/html/body/div[4]/button[2]")).click();
+    }
+
+    @And("^Resetting After editing person with \"([^\"]*)\" and \"([^\"]*)\" is not on the page\\.$")
+    public void resettingAfterEditingResettingPersonWithAndStatusOnThePage(String name, String job) throws Throwable {
+        assertNotEquals(driver.findElement(By.xpath("//*[@id=\"person0\"]/span[3]")).getText(), name);
+        assertNotEquals(driver.findElement(By.xpath("//*[@id=\"person0\"]/span[4]")).getText(), job);
+    }
+
+    @And("^Resetting After deleting person with \"([^\"]*)\" and \"([^\"]*)\" is on the page\\.$")
+    public void resettingAfterDeletingPersonWithAndStatusOnThePage(String name, String job) throws Throwable {
+        assertTrue(driver.findElement(By.xpath("//*[@id=\"person0\"]/span[3]")).isDisplayed());
+    }
+    @And("^Resetting After adding person with \"([^\"]*)\" and \"([^\"]*)\" is not on the page\\.$")
+    public void resettingAfterAddingPersonWithAndStatusOnThePage(String name, String job) throws Throwable {
+        assertEquals(0, driver.findElements(By.id("person3")).size());
+    }
+
+    @And("^I click Add person button$")
+    public void iClickAddPersonButton() throws Throwable{
+        driver.findElement(By.id("addPersonBtn")).click();
+    }
+
+    @And("^I enter name \"([^\"]*)\" and job \"([^\"]*)\"$")
+    public void iEnterNameAndJob(String name, String job) throws Throwable {
+        jobPage.enterNameAndJob(name,job);
+    }
+
+    @When("^I click clear all fields$")
+    public void iClickClearAllFields() {
+        driver.findElement(By.id("addPersonBtn")).click();
+    }
+
+    @Then("^Name and job fields are empty$")
+    public void nameAndJobFieldsAreEmpty() {
+        assertEquals("", driver.findElement(By.id("name")).getAttribute("value"));
+        assertEquals("", driver.findElement(By.id("job")).getAttribute("value"));
     }
 }
--- target/test-classes/features/Task2.feature
+++ target/test-classes/features/Task2.feature
@@ -1,15 +1,31 @@
 Feature: Task 2
 
-  Scenario Outline: New scenario for adding/deleting person and job
+  Scenario Outline: New scenario for adding/deleting/editing person and job
     Given I am on people/job page
     And I click <type> person
     Then Amount of people is <size>
+    And After <action> person with "<name>" and "<job>" <status> on the page.
     Examples:
-      | type   |  size |
-      | Add    |   4   |
-      | delete |   2   |
+      | type   |  size | action   | status  | name      |     job      |
+      | Add    |   4   | adding   |    is   | Vitalijs  | Engineer     |
+      | delete |   2   | deleting | is not  | Mike      | Web Designer |
+      | edit   |   3   | editing  |    is   | Vitalijs  | Engineer     |
 
-  Scenario: New scenario for editing person
+  Scenario Outline: New scenario for resetting list after adding/deleting/editing person and job
     Given I am on people/job page
-    When I click edit person
-    Then First persons name is "Vitalijs" and job is "Engineer"
+    And I click <type> person
+    And I click reset original list
+    Then Amount of people is <size>
+    And Resetting After <action> person with "<name>" and "<job>" <status> on the page.
+    Examples:
+      | type   |  size | action   | status  | name      |     job      |
+      | Add    |   3   | adding   | is not  | Vitalijs  | Engineer     |
+      | delete |   3   | deleting |   is    | Mike      | Web Designer |
+      | edit   |   3   | editing  | is not  | Vitalijs  | Engineer     |
+
+    Scenario: New scenario to check that clear button on adding a user works correctly
+      Given I am on people/job page
+      And I click Add person button
+      And I enter name "Vitalijs" and job "Engineer"
+      When I click clear all fields
+      Then Name and job fields are empty
